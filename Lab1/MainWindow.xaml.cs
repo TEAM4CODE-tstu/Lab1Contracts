7f4a0680746a58fc98b4a120c5946a646a09bb20
ilnikola07 <nikilushin07@gmail.com>
2026-09-22 17:28:01 +0300

    Добавлена кнопка для корректного выхода из приложения

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.Windows;
 
 namespace SortingAggregator
@@ -6,5 +7,28 @@ namespace SortingAggregator
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+
+        private bool _canClose = false;
+
+        /// <summary>
+        ///  Метод и предупреждение для выхода из приложения чтобы не по крестику
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, CancelEventArgs e) 
+        {
+            if (!_canClose)
+            {
+                e.Cancel = true; 
+                MessageBox.Show("Используйте кнопку 'Выход' для корректного закрытия приложения!", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            _canClose = true; 
+            Application.Current.Shutdown();
+        }
     }
 }
